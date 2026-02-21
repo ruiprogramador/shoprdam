3378b6eee82984f46d09780517999836254170ba
Rui2020pro <matosrui1998@gmail.com>
2026-02-21 08:56:17 +0000

    feat(toastification): Alerts

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -42,6 +42,12 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'flash' => [
+                'success' => fn() => $request->session()->get('success'),
+                'error'   => fn() => $request->session()->get('error'),
+                'info'    => fn() => $request->session()->get('info'),
+                'warning' => fn() => $request->session()->get('warning'),
+            ],
             'breadcrumbs' => $breadcrumbs,
             'test_value' => 'HELLO FROM MIDDLEWARE', // Test value
         ];
