b472989333b909227caa2a154c08c6098268ad73
Rui2020pro <matosrui1998@gmail.com>
2026-02-28 13:53:48 +0000

    Update dashboard to display user avatar

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Inertia::render('Dashboard/Index');
+        //dd($request->user()->image); // Debug the user's image path
+        return Inertia::render('Dashboard/Index', [
+            'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
+            'status' => session('status'),
+            'imageUrl' => $request->user()->image ? Storage::disk('public')->url($request->user()->image) : null,
+        ]);
     }
 }
