bfe8a75a52b9e4fedaae3f920e54cf263f7d9f1f
Rui2020pro <matosrui1998@gmail.com>
2026-03-29 09:59:31 +0100

    feat: implement user types system for users (backend)

---
+++ database/migrations/2026_03_29_082819_create_user_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        DB::table('user_types')->insert([
+            ['name' => 'User', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Vendor', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_types');
+    }
+};
