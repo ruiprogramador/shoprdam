46bba8461c12b4ea480220a1eb3f9ff8f0d53001
Rui2020pro <matosrui1998@gmail.com>
2026-04-25 19:09:16 +0100

    feat(kyc): update models - reviewer points to Admin, add KYC relation, helpers and status transitions

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\Admin as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class Admin extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $guard = 'admin';
+    protected $table = 'admins';
 
     /**
      * The attributes that are mass assignable.
